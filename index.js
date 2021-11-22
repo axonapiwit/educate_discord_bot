ee9c7aea4a52f271f869bd7ae94efa4a260cc3a1
apiwit <axon.apiwit@gmail.com>
2021-11-22 18:20:42 +0700

    fix  days

--- index.js
+++ index.js
@@ -33,13 +33,13 @@ client.on('messageCreate', msg => {
 
     const subjects = (day) => {
         if (day == 1) {
-            return msg.reply('225351[2] Computer and Network Security ความปลอดภัยบนคอมพิวเตอร์และเครือข่าย เวลา 15.00-17.00');
+            return msg.reply('225351[2] Computer and Network Security ความปลอดภัยบนคอมพิวเตอร์และเครือข่าย เวลา 13.00-15.00');
         }
         if (day == 2) {
             return msg.reply('227372[1] Software Processes กระบวนการซอฟต์แวร์ เวลา 09.00-12.00 \n225351[2] Computer and Network Security ความปลอดภัยบนคอมพิวเตอร์และเครือข่าย เวลา 13.00 - 15.00 \n227423[2] Preparation for Software Engineering Professional Experience การเตรียมพร้อมฝึกประสบการณ์วิชาชีพวิศวกรรมซอฟต์แวร์ เวลา 15.00 - 17.00');
         }
         if (day == 3) {
-            return msg.reply('227481[2] Software Quality and Measurement คุณภาพและการวัดซอฟต์แวร์ \nเวลา 13.00-16.00');
+            return msg.reply('227481[2] Software Quality and Measurement คุณภาพและการวัดซอฟต์แวร์ เวลา 13.00-16.00');
         }
         if (day == 4) {
             return msg.reply('227474[1] Software Evolution and Maintenance วิวัฒนาการและการบำรุงรักษาซอฟต์แวร์ เวลา 13.00-16.00 \n227321[2] Software Engineering Project I โครงงานทางวิศวกรรมซอฟต์แวร์ 1 เวลา 16.00-19.00');
